3d921c38413d3106c6dd5bb06b1b5271e86853e2
Other user <other-user@vmi1682372.contaboserver.net>
2024-06-01 08:35:54 -0500

    housekeeping

--- frontend/go/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-docker compose up -d
--- frontend/public/config.js
+++
@@ -1,4 +0,0 @@
-window.COMMA_URL_ROOT = 'https://connect-api.duckdns.org/';
-window.ATHENA_URL_ROOT = 'https://connect-api.duckdns.org/ws/';
-window.BILLING_URL_ROOT = 'https://billing.comma.ai/';
-window.USERADMIN_URL_ROOT = 'https://connect-useradmin.duckdns.org/';
