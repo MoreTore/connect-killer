f6f6a3d60e3d6e7a2ecd6c92ec775eaaa94b1aa5
Other user <other-user@vmi1682372.contaboserver.net>
2024-06-01 08:28:28 -0500

    housekeeping

--- frontend/dist/config.js
+++
@@ -1,4 +0,0 @@
-window.COMMA_URL_ROOT = 'http://localhost:3111/';
-window.ATHENA_URL_ROOT = 'http://localhost:3111/ws/';
-window.BILLING_URL_ROOT = 'https://billing.comma.ai/';
-window.USERADMIN_URL_ROOT = 'http://localhost:3112/useradmin';
