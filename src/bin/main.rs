26052e2f755917467bfb6271902cf90991fe7aff
MoreTore <ryleymccarroll22@gmail.com>
2024-08-30 20:47:28 +0200

    more threads

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,7 @@ use loco_rs::cli;
 use connect::app::App;
 use migration::Migrator;
 
-#[tokio::main(worker_threads = 2)]
+#[tokio::main(worker_threads = 24)]
 async fn main() -> eyre::Result<()> {
     cli::main::<App, Migrator>().await
 }
