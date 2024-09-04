5223965c33884b57a151771495060cc22ba4aecb
MoreTore <ryleymccarroll22@gmail.com>
2024-09-05 00:08:59 +0200

    Better auth error handling

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,10 +1,10 @@
 
-use std::collections::HashMap;
+use std::{env, collections::HashMap};
 
 use async_trait::async_trait;
 use axum::{
     extract::{FromRef, FromRequestParts, Query, ws::WebSocketUpgrade},
-    http::{request::Parts, HeaderMap}, response::{IntoResponse, Redirect},
+    http::{request::Parts, HeaderMap, header}, response::{IntoResponse, Redirect},
 };
 use axum_extra::extract::cookie;
 //use eyre::Error;
@@ -44,29 +44,45 @@ pub struct SuperUserJWT {
     pub user_model: Option<users::Model>,
 }
 
+use jsonwebtoken::errors::ErrorKind;
+
+#[derive(Serialize)]
+struct ErrorMessage {
+    code: u16,
+    message: String,
+    error_kind: Option<String>, // Include the ErrorKind as an optional field
+}
+
 #[derive(Debug, Error)]
 pub enum AuthError {
     #[error("unauthorized")]
-    Unauthorized,
+    Unauthorized(String),
     #[error("redirect to login")]
     RedirectToLogin,
     #[error("reset dongleId")]
     ResetDone,
-    #[error("jwt format error")]
-    FormatError,
     #[error("server error")]
     InternalError,
-
+    #[error("Error verifying jwt")]
+    JWTError(ErrorKind)
 }
 
 impl IntoResponse for AuthError {
     fn into_response(self) -> axum::response::Response {
         match self {
-            AuthError::Unauthorized => (http::StatusCode::UNAUTHORIZED, "Unauthorized").into_response(),
+            AuthError::Unauthorized(msg) => (http::StatusCode::UNAUTHORIZED, msg).into_response(),
             AuthError::RedirectToLogin => Redirect::to("/login").into_response(),
             AuthError::ResetDone => (http::StatusCode::ACCEPTED, "Reset your DongleId").into_response(),
-            AuthError::FormatError => (http::StatusCode::BAD_REQUEST, "Unauthorized").into_response(),
+            //AuthError::FormatError => (http::StatusCode::BAD_REQUEST, "Unauthorized").into_response(),
             AuthError::InternalError => (http::StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error").into_response(),
+            AuthError::JWTError(error_kind) => {
+                let error_message = ErrorMessage {
+                    code: http::StatusCode::UNAUTHORIZED.as_u16(),
+                    message: "JWT validation failed".to_string(),
+                    error_kind: Some(format!("{:?}", error_kind)), // Convert ErrorKind to a string
+                };
+                (http::StatusCode::UNAUTHORIZED, Json(error_message)).into_response()
+            }
         }
     }
 }
@@ -83,17 +99,15 @@ where
     async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
         let ctx: AppContext = AppContext::from_ref(state);
 
-        let token = extract_token(parts)
-            .map_err(|_| AuthError::RedirectToLogin)?;
+        let token = extract_token(parts)?;
+            //.map_err(|e| handle_unauth(parts, e))?;
 
         let jwt_secret = ctx.config.get_jwt_config().map_err(|_| AuthError::InternalError)?;
 
         let mut jwt_processor = jwt::JWT::new(&jwt_secret.secret);
 
-        let token_data = match jwt_processor.parse_unverified(&token) {
-            Ok(token_data) => token_data,
-            Err(_) => return Err(AuthError::RedirectToLogin),
-        };
+        let token_data = jwt_processor.parse_unverified(&token)
+            .map_err(|e| handle_jwt_err(parts, e.kind()))?;
 
         if let Ok(token_data) = jwt_processor.validate(&token) {
             // Try to find the user model by identity
@@ -115,9 +129,9 @@ where
                                 device_model: Some(device_model),
                             });
                         }
-                        Err(_) => {
+                        Err(e) => {
                             // If neither user model nor device model is found, return an unauthorized error
-                            return Err(AuthError::Unauthorized);
+                            return Err(AuthError::Unauthorized("Unauthorized".to_string()));
                         }
                     }
                 }
@@ -129,23 +143,24 @@ where
         let device = match devices::Model::find_device(&ctx.db, &token_data.claims.identity).await {
             Ok(device) => device,
             Err(e) => {
-                match e {
-                    ModelError::EntityNotFound => {
-                        let uri = parts.uri.clone();
-                        // if its a device trying to make a websocket connection, send dongle reset command thorugh athena.
-                        if uri.path().contains("/ws/v2/") { 
-                            let ws_upgrade = WebSocketUpgrade::from_request_parts(parts, state)
-                                .await
-                                .map_err(|_| AuthError::Unauthorized)?;
-                            ws_upgrade.on_upgrade(|socket| async move {
-                                crate::controllers::ws::send_reset(&ctx, socket).await;
-                            });
-                            return Err(AuthError::ResetDone);
-                        }
-                    }
-                    _ => () // db error other than not found
-                }
-                return Err(AuthError::Unauthorized);
+                // match e {
+                //     ModelError::EntityNotFound => {
+                //         let uri = parts.uri.clone();
+                //         // if its a device trying to make a websocket connection, send dongle reset command thorugh athena.
+                //         if uri.path().contains("/ws/v2/") { 
+                //             let ws_upgrade = WebSocketUpgrade::from_request_parts(parts, state)
+                //                 .await
+                //                 .map_err(|e| handle_unauth(parts, e))?;
+                //             ws_upgrade.on_upgrade(|socket| async move {
+                //                 crate::controllers::ws::send_reset(&ctx, socket).await;
+                //             });
+                //             return Err(AuthError::ResetDone);
+                //         }
+                //     }
+                //     _ => () // db error other than not found
+                // }
+                tracing::info!("Could not find device {}. Unauthorizded", token_data.claims.identity);
+                return Err(AuthError::Unauthorized("Unauthorized".to_string()));
             }
         };
         
@@ -153,7 +168,48 @@ where
             return Ok(Self { claims: token_data.claims, device_model: Some(device), user_model: None });
         }
 
-        return Err(AuthError::RedirectToLogin);
+        match jwt_processor.validate_pem(&token, device.public_key.as_bytes()) {
+            Ok(token_data) => return Ok(Self { claims: token_data.claims, device_model: Some(device), user_model: None }),
+            Err(e) => {                
+                Err(handle_jwt_err(parts, e.kind()))
+            }
+        }
+    }
+}
+
+fn handle_jwt_err(parts: &mut Parts, error_kind: &ErrorKind) -> AuthError {
+    let host_header = parts
+        .headers
+        .get(axum::http::header::HOST)
+        .and_then(|value| value.to_str().ok())
+        .unwrap_or_default();
+
+    let parts: Vec<&str> = host_header.split('.').collect();
+
+    if parts[0] == "useradmin" {
+        // Redirect for useradmin subdomain
+        return AuthError::RedirectToLogin;
+    } else {
+        // Return JWTError with the ErrorKind
+        return AuthError::JWTError(error_kind.clone());
+    }
+}
+
+fn handle_unauth(parts: &mut Parts, msg: &str) -> AuthError {
+    let host_header = parts
+        .headers
+        .get(axum::http::header::HOST)
+        .and_then(|value| value.to_str().ok())
+        .unwrap_or_default();
+
+    let parts: Vec<&str> = host_header.split('.').collect();
+
+    if parts[0] == "useradmin" {
+        // Redirect for useradmin subdomain
+        return AuthError::RedirectToLogin;
+    } else {
+        // Return JWTError with the ErrorKind
+        return AuthError::Unauthorized(msg.to_string());
     }
 }
 
@@ -166,7 +222,7 @@ pub struct DeviceClaims {
     pub exp: usize,
 }
 
-fn extract_token(parts: &Parts) -> Result<String, Error> {
+fn extract_token(parts: &mut Parts) -> Result<String, AuthError> {
     // Attempt to extract the token from the query string
     extract_token_from_query(QUERY_TOKEN_PREFIX, parts)
         .or_else(|_| {
@@ -176,7 +232,7 @@ fn extract_token(parts: &Parts) -> Result<String, Error> {
         .or_else(|_| {
             // If extracting from cookies fails, attempt to extract from the authorization header
             extract_token_from_header(&parts.headers)
-                .map_err(|e| Error::Unauthorized(e.to_string()))
+                .map_err(|e| handle_unauth(parts, &e))
         })
 }
 /// Function to extract a token from the authorization header
@@ -184,14 +240,14 @@ fn extract_token(parts: &Parts) -> Result<String, Error> {
 /// # Errors
 ///
 /// When token is not valid or out found
-pub fn extract_token_from_header(headers: &HeaderMap) -> Result<String, Error> {
+pub fn extract_token_from_header(headers: &HeaderMap) -> Result<String, String> {
     Ok(headers
         .get(AUTH_HEADER)
-        .ok_or_else(|| Error::Unauthorized(format!("header {AUTH_HEADER} token not found")))?
+        .ok_or_else(|| format!("Header {AUTH_HEADER} token not found"))?
         .to_str()
-        .map_err(|err| Error::Unauthorized(err.to_string()))?
+        .map_err(|err| err.to_string())?
         .strip_prefix(TOKEN_PREFIX)
-        .ok_or_else(|| Error::Unauthorized(format!("error strip {AUTH_HEADER} value")))?
+        .ok_or_else(|| format!("Error strip {AUTH_HEADER} value"))?
         .to_string())
 }
 
@@ -199,27 +255,27 @@ pub fn extract_token_from_header(headers: &HeaderMap) -> Result<String, Error> {
 ///
 /// # Errors
 /// when token value from cookie is not found
-pub fn extract_token_from_cookie(name: &str, parts: &Parts) -> Result<String, Error> {
+pub fn extract_token_from_cookie(name: &str, parts: &Parts) -> Result<String, String> {
 
     let jar: cookie::CookieJar = cookie::CookieJar::from_headers(&parts.headers);
     Ok(jar
         .get(name)
-        .ok_or(Error::Unauthorized("token is not found".to_string()))?
+        .ok_or("Token is not found".to_string())?
         .to_string()
         .strip_prefix(&format!("{name}="))
-        .ok_or_else(|| Error::Unauthorized("error strip value".to_string()))?
+        .ok_or_else(|| "Error strip value".to_string())?
         .to_string())
 }
 /// Extract a token value from query
 ///
 /// # Errors
 /// when token value from cookie is not found
-pub fn extract_token_from_query(name: &str, parts: &Parts) -> Result<String, Error> {
+pub fn extract_token_from_query(name: &str, parts: &Parts) -> Result<String, String> {
     // LogoResult
     let parameters: Query<HashMap<String, String>> =
-        Query::try_from_uri(&parts.uri).map_err(|err| Error::Unauthorized(err.to_string()))?;
+        Query::try_from_uri(&parts.uri).map_err(|err| err.to_string())?;
     parameters
         .get(name)
         .cloned()
-        .ok_or_else(|| Error::Unauthorized(format!("`{name}` query parameter not found")))
+        .ok_or_else(|| format!("`{name}` query parameter not found"))
 }
